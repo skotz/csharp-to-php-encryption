8f86f7f7b50d85fa12d28d2006fa2c02332671db
Scott Clayton <scottclayton84+github@gmail.com>
2015-05-10 12:56:22 -0500

    Fixed spelling, style, and naming problems

--- helpers/php-safe-key/PhpSafeKey/MainForm.Designer.cs
+++ helpers/php-safe-key/PhpSafeKey/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PhpSafeKey
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,49 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ofdPrivateKey = new System.Windows.Forms.OpenFileDialog();
+            this.sfdPrivateKey = new System.Windows.Forms.SaveFileDialog();
+            this.timerPopup = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // openFileDialog1
+            // ofdPrivateKey
             // 
-            this.openFileDialog1.FileName = "private.key";
-            this.openFileDialog1.Filter = "Private Key Files|*.key";
-            this.openFileDialog1.Title = "Select the provate key to convert:";
+            this.ofdPrivateKey.FileName = "private.key";
+            this.ofdPrivateKey.Filter = "Private Key Files|*.key";
+            this.ofdPrivateKey.Title = "Select a private key to encode into a PHP file.";
             // 
-            // saveFileDialog1
+            // sfdPrivateKey
             // 
-            this.saveFileDialog1.DefaultExt = "*.php";
-            this.saveFileDialog1.FileName = "private.php";
-            this.saveFileDialog1.Filter = "PHP Files|*.php";
-            this.saveFileDialog1.Title = "Where do you want to save the encoded private key?";
+            this.sfdPrivateKey.DefaultExt = "*.php";
+            this.sfdPrivateKey.FileName = "private.php";
+            this.sfdPrivateKey.Filter = "PHP Files|*.php";
+            this.sfdPrivateKey.Title = "Save the PHP encoded private key.";
             // 
-            // timer1
+            // timerPopup
             // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.timerPopup.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(10, 10);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "PHP Safe Key";
             this.TopMost = true;
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog ofdPrivateKey;
+        private System.Windows.Forms.SaveFileDialog sfdPrivateKey;
+        private System.Windows.Forms.Timer timerPopup;
     }
 }
 
---
+++ helpers/php-safe-key/PhpSafeKey/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PhpSafeKey
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            timerPopup.Start();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timerPopup.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerPopup.Stop();
+
+            DialogResult d = ofdPrivateKey.ShowDialog();
+            if (d == DialogResult.OK)
+            {
+                DialogResult s = sfdPrivateKey.ShowDialog();
+                if (s == DialogResult.OK)
+                {
+                    File.WriteAllText(sfdPrivateKey.FileName, "<?php $PrivateRSAKey = \"" + File.ReadAllText(ofdPrivateKey.FileName) + "\"; ?>");
+                    MessageBox.Show("The private key has been saved into a PHP file.");
+                }
+            }
+
+            this.Close();
+        }
+    }
+}
